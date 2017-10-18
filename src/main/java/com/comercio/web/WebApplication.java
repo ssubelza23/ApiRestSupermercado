7659f5055687b53634fae5cdb8f2d409e8561aa2
ssubelza23 <ssubelza23@gmail.com>
2017-10-18 07:09:44 -0400

    Productos v 1.0.0

--- src/main/java/com/comercio/web/WebApplication.java
+++ src/main/java/com/comercio/web/WebApplication.java
@@ -1,5 +1,7 @@
 package com.comercio.web;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,4 +11,5 @@ public class WebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
+
 }
